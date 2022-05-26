6b82c8fff4ff37e7eff628cfc5ec62392c08e9cc
Shakal1985 <shakal1985@i.ua>
2022-05-27 00:30:04 +0300

    second

--- clean.py
+++ clean.py
@@ -4,87 +4,71 @@ import os
 import shutil
 from threading import Thread
 
-
 # Creating normalisation dictionary TRANS
-
 CYRILLIC_SYMBOLS = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяєіїґ"
 TRANSLATION = ("a", "b", "v", "g", "d", "e", "e", "j", "z", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u",
                "f", "h", "ts", "ch", "sh", "sch", "", "y", "", "e", "yu", "u", "ja", "je", "ji", "g")
 
 TRANS = {}
-for c, l in zip(CYRILLIC_SYMBOLS, TRANSLATION):
-    TRANS[ord(c)] = l
-    TRANS[ord(c.upper())] = l.upper() 
+for cyrillic, latin in zip(CYRILLIC_SYMBOLS, TRANSLATION):
+    TRANS[ord(cyrillic)] = latin
+    TRANS[ord(cyrillic.upper())] = latin.upper() 
+
 
 # Check if Path exists
-
 def path_verification(path_to_folder):
     path_to_folder=rf'{path_to_folder}'
-    i=0
+    number_of_attemps=0
     
-    while i<10:
-        i+=1 
-        p = pl.Path(path_to_folder)
+    while number_of_attemps<10:
+        number_of_attemps+=1 
+        path = pl.Path(path_to_folder)
         
-        if not p.exists() or p.is_file():
-            print(f'Please enter the valid path to folder\nYou have {9-i} attempts')
+        if not path.exists() or path.is_file():
+            print(f'Please enter the valid path to folder\nYou have {9-number_of_attemps} attempts')
             path_to_folder = input('Enter path to folder:')
 
-        if i==10:
+        if number_of_attemps==10:
             break         
 
-        if p.exists():
-            path_to_folder_1=rf'{path_to_folder}' 
-            return path_to_folder_1
+        if path.exists():
+            valid_path_to_folder=rf'{path_to_folder}' 
+            return valid_path_to_folder
+
         
 # List output of folders and files. Empty folders remove                 
-def find_files(path_to_folder_1, founded_files=[], founded_folders=[]):
-
-          
-    p = pl.Path(path_to_folder_1) 
+def find_files(valid_path_to_folder, founded_files=[], founded_folders=[]):       
+    path = pl.Path(valid_path_to_folder) 
                           
-    for i,j in enumerate(p.iterdir()):
-        
+    for elements in path.iterdir():     
+    
+        if elements.is_file():
+            pure_path=pl.PurePath(pl.Path(elements))
+            pure_path=str(pure_path)                             
+            path_without_suffix_and_name=pure_path.replace(elements.suffix, '')
+            path_without_suffix_and_name=pure_path.replace(elements.name, '')
+            elements_name_without_suffix = elements.name.replace(elements.suffix, '')        
+            founded_files.append([elements_name_without_suffix, elements.suffix, path_without_suffix_and_name])                     
+ 
+        if elements.is_dir():            
+            founded_folders.append(os.fspath(elements))
+            path_to_folder_iterate=pl.Path(elements)
+            thread_find_folders = Thread(target=find_files, args=(path_to_folder_iterate,))
+            thread_find_folders.start()
        
-        if j.is_file():
-            a=pl.PurePath(pl.Path(j))
-            a=str(a)                             
-            b=a.replace(j.suffix, '')
-            b=a.replace(j.name, '')            
-                      
-            founded_files.append([j.name.replace(j.suffix, ''), j.suffix, b])
-            
-            
-            
-        if j.is_dir():
-            
-            founded_folders.append(os.fspath(j))
-            path_to_folder_2=pl.Path(j)
-
-            thread = Thread(target=find_files, args=(path_to_folder_2,))
-            thread.start()
-            
-            
-        
-
-        
-    #print(f'Please see unsorted list of folders and files bellow: ')
-    #print(founded_files)
-       
-    return path_to_folder_1, founded_files, founded_folders
-
+    return valid_path_to_folder, founded_files, founded_folders
    
 
-def normalize(founded_files,founded_folders, path_to_folder_1):     
+def normalize(founded_files,founded_folders, valid_path_to_folder):     
     
     founded_files_normalized=[]
     founded_folders_normalized=[]
-    c = len(path_to_folder_1)+1 
-
+    path_length = len(valid_path_to_folder)+1 
     
     for i in founded_files:
         if 'archives' in i[2] or 'video' in i[2] or 'audio' in i[2] or 'documents' in i[2] or 'images' in i[2]:
             founded_files_normalized.append([fr"{i[0]}", fr"{i[1]}", fr"{i[2]}"])
+
         else:    
             a=i[0] 
             a=i[0].translate(TRANS)
@@ -92,66 +76,60 @@ def normalize(founded_files,founded_folders, path_to_folder_1):
             founded_files_normalized.append([fr"{d}", fr"{i[1]}", fr"{i[2]}"])
 
     for j in founded_folders:
+
         if 'archives' in j or 'video' in j or 'audio' in j or 'documents' in j or 'images' in j:
             founded_folders_normalized.append(fr'{j}')
+
         else:
-            ss=j[c:].translate(TRANS)
-            #print(ss)
-            ss=re.sub(r'[^\w^\\]', '_', fr"{ss}")
-            founded_folders_normalized.append(fr'{j[0:c]}{ss}')
-      
+            translated_path=j[path_length:].translate(TRANS)            
+            translated_and_normalized_path=re.sub(r'[^\w^\\]', '_', fr"{translated_path}")
+            founded_folders_normalized.append(fr'{j[0:path_length]}{translated_and_normalized_path}')     
           
-    return   founded_files_normalized, founded_folders_normalized
+    return  founded_files_normalized, founded_folders_normalized
 
 
 def files_rename(founded_files_normalized, founded_files):
-    for  item1, item2 in zip(founded_files, founded_files_normalized): 
+    for  founded_file, founded_file_normalized in zip(founded_files, founded_files_normalized): 
     
-        if item1[0]!=item2[0]:
-            
-            os.rename( fr'{item1[2]}{item1[0]}{item1[1]}', fr'{item1[2]}{item2[0]}{item2[1]}')
-            #print(item1[0])
-            #print(item2[0])
+        if founded_file[0]!=founded_file_normalized[0]:
+            os.rename( fr'{founded_file[2]}{founded_file[0]}{founded_file[1]}', fr'{founded_file[2]}{founded_file_normalized[0]}{founded_file_normalized[1]}')
 
 
 def folders_rename(founded_folders, founded_folders_normalized):
    
-    for  item3, item4 in zip(founded_folders, founded_folders_normalized):
+    for  founded_folder, founded_folder_normalized in zip(founded_folders, founded_folders_normalized):
         
-        if item3!=item4:
-            print(item3)
-            print(item4)
-            os.rename( f'{item3}', f'{item4}')   
+        if founded_folder!=founded_folder_normalized:
+            print(founded_folder)
+            print(founded_folder_normalized)
+            os.rename( f'{founded_folder}', f'{founded_folder_normalized}')   
+
 
 def files_collect_images(founded_files, path_to_folder):
     list_of_images=[]
     for k in founded_files:
-        
-        
-        if (k[1].casefold()=='.JPEG'.casefold() or k[1].casefold()=='.PNG'.casefold() or k[1].casefold()=='.JPG'.casefold() or k[1].casefold()=='.SVG'.casefold())\
-            and fr"{path_to_folder}\images" not in  fr"{k[2]}":
-             
+
+        if (k[1].casefold()=='.JPEG'.casefold() or k[1].casefold()=='.PNG'.casefold() or k[1].casefold()=='.JPG'.casefold() or 
+        k[1].casefold()=='.SVG'.casefold()) and fr"{path_to_folder}\images" not in  fr"{k[2]}":            
             list_of_images.append(f"{k[0]}{k[1]}")
+
             if not pl.Path(fr"{path_to_folder}\images").exists():
-
                 os.mkdir(fr'{path_to_folder}\images')
-               
 
-            
             os.replace(fr'{k[2]}{k[0]}{k[1]}' , fr"{path_to_folder}\images\{k[0]}{k[1]}")
     print(f"List of images: {list_of_images}")    
+
 
 def files_collect_video(founded_files, path_to_folder):
     list_of_video=[]
     for k in founded_files:
         
-        if (k[1].casefold()=='.AVI'.casefold() or k[1].casefold()=='.MP4'.casefold() or k[1].casefold()=='.MOV'.casefold() or k[1].casefold()=='.MKV'.casefold())\
-             and fr"{path_to_folder}\video" not in  fr"{k[2]}":
+        if (k[1].casefold()=='.AVI'.casefold() or k[1].casefold()=='.MP4'.casefold() or k[1].casefold()=='.MOV'.casefold() or
+        k[1].casefold()=='.MKV'.casefold()) and fr"{path_to_folder}\video" not in  fr"{k[2]}":
 
             list_of_video.append(f"{k[0]}{k[1]}")
             if not pl.Path(fr"{path_to_folder}\video").exists():
                 os.mkdir(fr'{path_to_folder}\video')
-
            
             os.replace(fr'{k[2]}{k[0]}{k[1]}' , fr"{path_to_folder}\video\{k[0]}{k[1]}")
     print(f"List of video: {list_of_video}") 
@@ -161,30 +139,28 @@ def files_collect_documents(founded_files, path_to_folder):
     list_of_documents=[]
     for k in founded_files:
         
-        if (k[1].casefold()=='.DOC'.casefold() or k[1].casefold()=='.DOCX'.casefold() or k[1].casefold()=='.TXT'.casefold() or k[1].casefold()=='.PDF'.casefold()) \
-            or k[1].casefold()=='.XLSX'.casefold() or k[1].casefold()=='.PPTX'.casefold() and fr"{path_to_folder}\documents" not in  fr"{k[2]}":
+        if fr"{path_to_folder}\documents" not in fr"{k[2]}" and (k[1].casefold()=='.DOC'.casefold() or
+        k[1].casefold()=='.DOCX'.casefold() or k[1].casefold()=='.TXT'.casefold() or
+        k[1].casefold()=='.PDF'.casefold()) or k[1].casefold()=='.XLSX'.casefold() or k[1].casefold()=='.PPTX'.casefold():           
             list_of_documents.append(f"{k[0]}{k[1]}")
             
-
             if not pl.Path(fr"{path_to_folder}\documents").exists():
-                os.mkdir(fr'{path_to_folder}\documents')
-
-                
+                os.mkdir(fr'{path_to_folder}\documents')                
             
             os.replace(fr'{k[2]}{k[0]}{k[1]}' , fr"{path_to_folder}\documents\{k[0]}{k[1]}")          
     print(f"List of documents: {list_of_documents}")  
+
     
 def files_collect_audio(founded_files, path_to_folder):
     list_of_audio=[]
     for k in founded_files:
         
-        if (k[1].casefold()=='.MP3'.casefold() or k[1].casefold()=='.OGG'.casefold() or k[1].casefold()=='.WAV'.casefold() or k[1].casefold()=='.AMR'.casefold())\
-              and fr"{path_to_folder}\audio" not in  fr"{k[2]}":
-
+        if (k[1].casefold()=='.MP3'.casefold() or k[1].casefold()=='.OGG'.casefold() or k[1].casefold()=='.WAV'.casefold()
+        or k[1].casefold()=='.AMR'.casefold()) and fr"{path_to_folder}\audio" not in  fr"{k[2]}":
             list_of_audio.append(f"{k[0]}{k[1]}")
+
             if not pl.Path(fr"{path_to_folder}\audio").exists():
                 os.mkdir(fr'{path_to_folder}\audio')
-
             
             os.replace(fr'{k[2]}{k[0]}{k[1]}' , fr"{path_to_folder}\audio\{k[0]}{k[1]}")  
     print(f"List of audio: {list_of_audio}")  
@@ -193,22 +169,15 @@ def files_collect_archives(founded_files, path_to_folder):
     list_of_archives=[]
     for k in founded_files:
         
-        if (k[1].casefold()=='.ZIP'.casefold() or k[1].casefold()=='.GZ'.casefold() or k[1].casefold()=='.TAR'.casefold())  and fr"{path_to_folder}\archives" not in  fr"{k[2]}":
+        if (k[1].casefold()=='.ZIP'.casefold() or k[1].casefold()=='.GZ'.casefold() or k[1].casefold()=='.TAR'.casefold())\
+        and fr"{path_to_folder}\archives" not in  fr"{k[2]}":
             list_of_archives.append(f"{k[0]}{k[1]}") 
 
             if not pl.Path(fr"{path_to_folder}\archives").exists():
-                os.mkdir(fr'{path_to_folder}\archives')
-
-            
-            #os.replace(fr'{k[2]}{k[0]}{k[1]}' , fr"{path_to_folder}\archives\{k[0]}{k[1]}")
-            
-            shutil.unpack_archive(fr'{k[2]}{k[0]}{k[1]}',  fr"{path_to_folder}\archives\{k[0]}")
-             
+                os.mkdir(fr'{path_to_folder}\archives')            
+                        
+            shutil.unpack_archive(fr'{k[2]}{k[0]}{k[1]}',  fr"{path_to_folder}\archives\{k[0]}")             
     print(f"List of archives: {list_of_archives}")  
-
-       
-
-
 
 
 def files_collect_other_files(founded_files, path_to_folder):
@@ -218,15 +187,20 @@ def files_collect_other_files(founded_files, path_to_folder):
     list_of_other_files=[]
     for k in founded_files:
         list_of_known_suffix.add(k[1])
+
         if 'archives' in k[2] or 'video' in k[2] or 'audio' in k[2] or 'documents' in k[2] or 'images' in k[2]:
             continue
+
         elif    k[1].casefold()!='.ZIP'.casefold()  and k[1].casefold()!='.GZ'.casefold()   and k[1].casefold()!='.TAR'.casefold()\
-            and k[1].casefold()!='.MP3'.casefold()  and k[1].casefold()!='.OGG'.casefold()  and k[1].casefold()!='.WAV'.casefold()  and k[1].casefold()!='.AMR'.casefold()\
-            and k[1].casefold()!='.DOC'.casefold()  and k[1].casefold()!='.DOCX'.casefold() and k[1].casefold()!='.TXT'.casefold()  and k[1].casefold()!='.PDF'.casefold()\
-            and k[1].casefold()!='.XLSX'.casefold() and k[1].casefold()!='.PPTX'.casefold() and k[1].casefold()!='.AVI'.casefold()  and k[1].casefold()!='.MP4'.casefold()\
-            and k[1].casefold()!='.MOV'.casefold()  and k[1].casefold()!='.MKV'.casefold()  and k[1].casefold()!='.JPEG'.casefold() and k[1].casefold()!='.PNG'.casefold()\
-            and k[1].casefold()!='.JPG'.casefold()  and k[1].casefold()!='.SVG'.casefold() :      
+            and k[1].casefold()!='.MP3'.casefold()  and k[1].casefold()!='.OGG'.casefold()  and k[1].casefold()!='.WAV'.casefold()\
+            and k[1].casefold()!='.AMR'.casefold()  and k[1].casefold()!='.DOC'.casefold()  and k[1].casefold()!='.DOCX'.casefold()\
+            and k[1].casefold()!='.TXT'.casefold()  and k[1].casefold()!='.PDF'.casefold()  and k[1].casefold()!='.XLSX'.casefold()\
+            and k[1].casefold()!='.PPTX'.casefold() and k[1].casefold()!='.AVI'.casefold()  and k[1].casefold()!='.MP4'.casefold()\
+            and k[1].casefold()!='.MOV'.casefold()  and k[1].casefold()!='.MKV'.casefold()  and k[1].casefold()!='.JPEG'.casefold()\
+            and k[1].casefold()!='.PNG'.casefold()  and k[1].casefold()!='.JPG'.casefold()  and k[1].casefold()!='.SVG'.casefold() :      
+            
             list_of_other_files.append(f"{k[0]}{k[1]}")
+            
             if not pl.Path(fr"{path_to_folder}\other_files").exists():
                 os.mkdir(fr'{path_to_folder}\other_files')
             list_of_unknown_suffix.add(k[1])
@@ -238,75 +212,48 @@ def files_collect_other_files(founded_files, path_to_folder):
     print (f"List of known suffix: {list_of_known_suffix}\nList of unknown suffix: {list_of_unknown_suffix}")       
 
 
-def del_empty_dirs(path_to_folder_1):
-    path = pl.Path(path_to_folder_1)
+def del_empty_dirs(valid_path_to_folder):
+    path = pl.Path(valid_path_to_folder)
     
-    for d in os.listdir(path):
-        a = os.path.join(path, d)
+    for items in os.listdir(path):
+        item_path = os.path.join(path, items)
 
-        if os.path.isdir(a):
-            del_empty_dirs(a)
+        if os.path.isdir(item_path):
+            del_empty_dirs(item_path)
 
-            if not os.listdir(a) and 'archives' not in a and 'video' not in a and 'audio' not in a and 'documents' not in a and 'images' not in a:
-                os.rmdir(a)
+            if not os.listdir(item_path) and 'archives' not in item_path and 'video' not in item_path\
+            and 'audio' not in item_path and 'documents' not in item_path and 'images' not in item_path:
+                os.rmdir(item_path)
 
 def clean():
     
     path_to_folder = input('Enter path to folder:')   
-    path_to_folder_1=path_verification(path_to_folder)
-    
-    
-     
-    path_to_folder_1, founded_files, founded_folders = find_files(path_to_folder_1)
-
-    
-    founded_files_normalized, founded_folders_normalized= normalize(founded_files,founded_folders, path_to_folder_1)
+    valid_path_to_folder=path_verification(path_to_folder)         
+    valid_path_to_folder, founded_files, founded_folders = find_files(valid_path_to_folder)    
+    founded_files_normalized, founded_folders_normalized= normalize(founded_files,founded_folders, valid_path_to_folder)
 
     files_rename(founded_files_normalized, founded_files)
-    founded_files.clear()
-
-    founded_files_normalized.clear()
-    founded_folders_normalized.clear()
-    founded_folders.clear()
-
-
-    path_to_folder_1=path_verification(path_to_folder)
-    
-    path_to_folder_1, founded_files, founded_folders = find_files(path_to_folder_1)
-
-
-    founded_files_normalized, founded_folders_normalized= normalize(founded_files,founded_folders, path_to_folder_1)
-
     folders_rename(founded_folders, founded_folders_normalized)
-
-    founded_folders_normalized.clear()
-    founded_folders.clear()
-    founded_files_normalized.clear()
-    founded_files.clear()
-    
-    path_to_folder_1, founded_files, founded_folders = find_files(path_to_folder_1)
-
-
-    thread_img = Thread(target=files_collect_images(founded_files, path_to_folder))
+ 
+    thread_img = Thread(target=files_collect_images(founded_files_normalized, valid_path_to_folder))
     thread_img.start()
     
-    thread_video = Thread(target=files_collect_video(founded_files, path_to_folder))
+    thread_video = Thread(target=files_collect_video(founded_files_normalized, valid_path_to_folder))
     thread_video.start()
 
-    thread_doc = Thread(target=files_collect_documents(founded_files, path_to_folder))
+    thread_doc = Thread(target=files_collect_documents(founded_files_normalized, valid_path_to_folder))
     thread_doc.start()
 
-    thread_audio = Thread(target=files_collect_audio(founded_files, path_to_folder))
+    thread_audio = Thread(target=files_collect_audio(founded_files_normalized, valid_path_to_folder))
     thread_audio.start()
 
-    thread_arch = Thread(target=files_collect_archives(founded_files, path_to_folder))
+    thread_arch = Thread(target=files_collect_archives(founded_files_normalized, valid_path_to_folder))
     thread_arch.start()
     
-    thread_other = Thread(target=files_collect_other_files(founded_files, path_to_folder))
-    thread_other.start()
-    
+    thread_other = Thread(target=files_collect_other_files(founded_files_normalized, valid_path_to_folder))
+    thread_other.start()    
 
-    del_empty_dirs(path_to_folder_1)
+    del_empty_dirs(valid_path_to_folder)
 
 
 clean()
